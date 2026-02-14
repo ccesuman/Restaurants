ed2a1c40773c0cbb533358b0337ca2bd5854a36e
ccesuman <ccesuman@gmail.com>
2026-02-14 14:31:19 +0530

    (feat):  Setup the project

---
+++ Restaurants/Restaurants.Application/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Restaurants.Application.Restaurants;
+
+namespace Restaurants.Application.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IRestaurantsService, RestaurantsService>();
+        }
+    }
+}
---
+++ Restaurants/Restaurants.Application/Restaurants/RestaurantsService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Repositories;
+
+namespace Restaurants.Application.Restaurants;
+
+internal class RestaurantsService(IRestaurantsRepository restaurantsRepository, ILogger<RestaurantsService> logger)
+    : IRestaurantsService
+{
+    public async Task<IEnumerable<Restaurant>> GetAllRestaurantsAsync()
+    {
+        logger.LogInformation("Getting all restaurants");
+       var restaurants = await restaurantsRepository.GetAllAsync();
+       return restaurants;
+    }
+}
