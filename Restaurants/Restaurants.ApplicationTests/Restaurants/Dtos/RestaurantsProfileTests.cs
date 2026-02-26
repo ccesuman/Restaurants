fae7a2e097cc7e1f79acc4709e635a85d18187cc
ccesuman <ccesuman@gmail.com>
2026-02-26 14:08:23 +0530

    (test): Added test for RestaurantsProfile to check auto mapper

---
+++ Restaurants/Restaurants.ApplicationTests/Restaurants/Dtos/RestaurantsProfileTests.cs
@@ -0,0 +1,120 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Restaurants.Application.Restaurants.Commands.CreateRestaurant;
+using Restaurants.Application.Restaurants.Commands.UpdateRestaurant;
+using Restaurants.Application.Restaurants.Dtos;
+using Restaurants.Domain.Entities;
+using Xunit;
+
+namespace Restaurants.ApplicationTests.Restaurants.Dtos;
+
+public class RestaurantsProfileTests
+{
+    private readonly IMapper _mapper;
+
+    public RestaurantsProfileTests()
+    {
+        var configuration = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<RestaurantsProfile>();
+        });
+
+         _mapper = configuration.CreateMapper();
+    }
+    [Fact()]
+    public void CreateMap_ForRestaurantToRestaurantDto_MapsCorrectly()
+    {
+        // Arrange
+        var restaurant = new Restaurant()
+        {
+            Id = 1,
+            Name = "Test Restaurant",
+            Description = "A test restaurant",
+            Category = "Test Category",
+            HasDelivery = true,
+            ContactEmail = "test@example.com",
+            ContactNumber = "1234567890",
+            Address = new Address()
+            {
+                Street = "123 Test St",
+                City = "Test City",
+                PostalCode = "12345"
+            }
+        };
+
+        // Act
+        var restaurantDto = _mapper.Map<RestaurantDto>(restaurant);
+
+        // Assert
+        restaurantDto.Should().NotBeNull();
+        restaurantDto.Id.Should().Be(restaurant.Id);
+        restaurantDto.Name.Should().Be(restaurant.Name);
+        restaurantDto.Description.Should().Be(restaurant.Description);
+        restaurantDto.Category.Should().Be(restaurant.Category);
+        restaurantDto.HasDelivery.Should().Be(restaurant.HasDelivery);
+        restaurantDto.ContactEmail.Should().Be(restaurant.ContactEmail);
+        restaurantDto.ContactNumber.Should().Be(restaurant.ContactNumber);
+        restaurantDto.City.Should().Be(restaurant.Address?.City);
+        restaurantDto.Street.Should().Be(restaurant.Address?.Street);
+        restaurantDto.PostalCode.Should().Be(restaurant.Address?.PostalCode);
+    }
+
+    [Fact]
+    public void CreateMap_ForCreateRestaurantCommandToRestaurant_MapsCorrectly()
+    {
+        // Arrange
+        var command = new CreateRestaurantCommand()
+        {
+            Name = "Test Restaurant",
+            Description = "A test restaurant",
+            Category = "Test Category",
+            HasDelivery = true,
+            ContactEmail = "test@example.com",
+            ContactNumber = "1234567890",
+            Street = "123 Test St",
+            City = "Test City",
+            PostalCode = "12345"
+        };
+
+        // Act
+        var restaurant = _mapper.Map<Restaurant>(command);
+
+        // Assert
+        restaurant.Should().NotBeNull();
+        restaurant.Name.Should().Be(command.Name);
+        restaurant.Description.Should().Be(command.Description);
+        restaurant.Category.Should().Be(command.Category);
+        restaurant.HasDelivery.Should().Be(command.HasDelivery);
+        restaurant.ContactEmail.Should().Be(command.ContactEmail);
+        restaurant.ContactNumber.Should().Be(command.ContactNumber);
+        restaurant.Address?.City.Should().Be(command.City);
+        restaurant.Address?.Street.Should().Be(command.Street);
+        restaurant.Address?.PostalCode.Should().Be(command.PostalCode);
+    }
+
+
+    public void CreateMap_ForUpdateRestaurantCommandToRestaurant_MapsCorrectly()
+    {
+        // Arrange
+        var command = new UpdateRestaurantCommand()
+        {
+            Id=1,
+            Name = "Test Restaurant",
+            Description = "A test restaurant",
+            Category = "Test Category",
+            HasDelivery = true,
+        };
+
+        // Act
+        var restaurant = _mapper.Map<Restaurant>(command);
+
+        // Assert
+        restaurant.Should().NotBeNull();
+        restaurant.Id.Should().Be(command.Id);
+        restaurant.Name.Should().Be(command.Name);
+        restaurant.Description.Should().Be(command.Description);
+        restaurant.Category.Should().Be(command.Category);
+        restaurant.HasDelivery.Should().Be(command.HasDelivery);
+    }
+
+}
